47af5f3525236b21c324b204d965955a39905fb6
Omkar Raykar <raykaromkar1244@gmail.com>
2024-09-30 03:51:13 +0530

    portfolio done

--- server/app.js
+++ server/app.js
@@ -4,10 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = [
+  "https://portfolio-md6i45k0j-omkar-raykars-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    origin: allowedOrigins,
+    credentials: true, // Allow cookies to be sent with requests
   })
 );
 
@@ -16,11 +20,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// routes import
+// Routes import
 import contactRouter from "./routes/contactRoutes.js";
 
-// routes declaration
+// Routes declaration
 app.use("/contact", contactRouter);
-
 
 export { app };
